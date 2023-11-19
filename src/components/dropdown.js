09ab28f87855b69c6111a0c35cbfc300feabedb2
stevenshow <steven.schoebinger@gmail.com>
2023-11-18 23:01:49 -0700

    make bubble larger

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -17,7 +17,7 @@ export default function DropDown({ selected, setSelected }) {
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-slate-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 <span
-                  className={`inline-block h-2 w-2 flex-shrink-0 rounded-full ${getBubbleColor(
+                  className={`inline-block h-3 w-3 flex-shrink-0 rounded-full ${getBubbleColor(
                     selected,
                   )}`}
                 />
@@ -54,7 +54,7 @@ export default function DropDown({ selected, setSelected }) {
                       <>
                         <div className="flex items-center">
                           <span
-                            className={`inline-block h-2 w-2 flex-shrink-0 rounded-full ${getBubbleColor(
+                            className={`inline-block h-3 w-3 flex-shrink-0 rounded-full ${getBubbleColor(
                               team,
                             )}`}
                             aria-hidden="true"
